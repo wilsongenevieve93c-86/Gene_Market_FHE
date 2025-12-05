a3888d7aa49709431fbdddf0cc8f0b46ed2cd796
wilsongenevieve93c-86 <m_kovalevskaya_38262817@rambler.ru>
2025-12-05 09:22:40 +0800

    add window.d.ts, ZamaConfig.sol, payment-import-demo.rst

---
+++ mutations-upload-gateway/inventory-fragments.java
@@ -0,0 +1,2 @@
+// Auto-generated file: D:\MyCode\zama\demo\mutations-upload-gateway\inventory-fragments.java
+// Generated at: 2025-10-24T11:21:09.551Z
---
+++ mutations-upload-gateway/payment-import-demo.rst
@@ -0,0 +1,2 @@
+// Auto-generated file: D:\MyCode\zama\demo\mutations-upload-gateway\payment-import-demo.rst
+// Generated at: 2025-10-24T11:21:09.552Z
